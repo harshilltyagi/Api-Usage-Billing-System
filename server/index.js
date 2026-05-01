ac550b854103a5ead6569b8ca7797d8649133ca0
Harshill Tyagi <82799112+harshilltyagi@users.noreply.github.com>
2026-05-01 15:09:56 +0530

    Update index.js
    changes in cors

--- server/index.js
+++ server/index.js
@@ -12,6 +12,11 @@ import adminRoutes from "./routes/adminRoutes.js";
 const app = express();
 
 app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
